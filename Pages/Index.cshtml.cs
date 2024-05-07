068abbe6c9cec061063378d7ef772c9a47fb7120
Tiffenie Mashburn <trmashburn1@buffs.wtamu.edu>
2024-05-07 16:30:07 -0500

    Saving

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using VideoGame.Models;
 
 namespace VideoGame.Pages;
@@ -10,6 +11,7 @@ public class IndexModel : PageModel
     private readonly GameDbContext _context;
     private readonly ILogger<IndexModel> _logger;
     public List<Game> Games {get; set;}  = default!;
+    public SelectList GamesDropDown {get; set;}  = default!;
 
     public IndexModel(GameDbContext context, ILogger<IndexModel> logger)
     {
@@ -20,5 +22,6 @@ public class IndexModel : PageModel
     public void OnGet()
     {
         Games = _context.Games.ToList();
+        GamesDropDown = new SelectList(Games, "GameId", "Title");
     }
 }
