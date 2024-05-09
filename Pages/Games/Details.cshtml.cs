9f80aac3bedadd8bb4230a0e97aeeed46b80ee96
Tiffenie Mashburn <trmashburn1@buffs.wtamu.edu>
2024-05-09 16:31:40 -0500

    save

--- Pages/Games/Details.cshtml.cs
+++ Pages/Games/Details.cshtml.cs
@@ -19,6 +19,8 @@ namespace VideoGame.Pages.Games
         }
 
         public Game Game { get; set; } = default!;
+        [BindProperty]
+        public Character Character { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -38,5 +40,60 @@ namespace VideoGame.Pages.Games
             }
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var game = await _context.Games.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            // Associate character with the game
+            Character.GameId = game.GameId;
+
+            // Save character to database
+            _context.Characters.Add(Character);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Details", new { id });
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int? gameId, int? characterId)
+        {
+            if (gameId == null || characterId == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Games.FindAsync(gameId);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Characters.FindAsync(characterId);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            _context.Characters.Remove(character);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Details", new { id = gameId });
+        }
     }
 }
+
+
